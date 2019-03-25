a87fbab78fb1c985532ee38d07e1380d2fc2e897
GodWithegg <675175647@qq.com>
2019-03-25 23:10:24 +0800

    建造者模式

--- designedparttern/designedParrtern/src/com/danhuang/builder/Client.java
+++ designedparttern/designedParrtern/src/com/danhuang/builder/Client.java
@@ -1,5 +1,14 @@
 package com.danhuang.builder;
 
+/**
+ * 建造者模式
+ * 使用多个简单的的对象一步步构建成一个复杂的对象
+ * 这个项目我的理解是：
+ * 	一个飞船有多个功能，这些功能分别对应不同的对象，这些功能分为两个接口，一个是建造飞船部件对象的接口（Builder)，一个是建造飞船部件功能的接口(Director)
+ * 		然后用两个类分别实现接口。这便是建造者模式的用意，将复杂的功能单独分开，各自实现。
+ * @author danhuang
+ *
+ */
 public class Client {
 	public static void main(String[] args) {
 		AirShipDirector airShipDirector = new SxtAirShipDirector(new SxtAirShipBuilder());
